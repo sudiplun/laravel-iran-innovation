9b39b68efa8311d2a055daf6c310301d06dff867
lun <sudeeplun@gmail.com>
2024-01-17 22:59:47 +0545

    controller group

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::controller(Frontend::class)->group(function () {
 
-Route::get('/', [Frontend::class, 'index'])
-    ->name('index');
+    Route::get('/', 'index')
+        ->name('index');
 
-Route::get('/contact', [Frontend::class, 'contact'])
-    ->name('mediplus.contact');
+    Route::get('/contact', 'contact')
+        ->name('mediplus.contact');
 
-Route::get('/blog-single', [Frontend::class, 'blog'])
-    ->name('mediplus.blog');
+    Route::get('/blog-single', 'blog')
+        ->name('mediplus.blog');
+});
